082bb3b88f6912738ee95a0d93a92d2b4f4903c9
Stefan Scherer <scherer_stefan@icloud.com>
2016-01-08 16:32:19 +0100

    Add minimal test

--- test.ps1
+++ test.ps1
@@ -41,53 +41,16 @@ $zip.Dispose()
 "TEST: Installation of package should work"
 . choco install -y docker-machine-vmwareworkstation $options -source .
 
-"TEST: Create a machine with driver none should work"
-. docker-machine create -d vmwareworkstation test
-
-"TEST: List should show the machine"
-. docker-machine ls
-if ($(docker-machine ls).Contains("test   -        none             http://127.0.0.1:2376")) {
-  Write-Error "FAIL: machine not found"
-}
-
-"TEST: Remove the machine should work"
-. docker-machine rm -f test
-
-"TEST: Uninstall show remove the binary"
-. choco uninstall -y docker-machine
+"TEST: Create a machine with driver vmwareworkstation should work"
 try {
-  . docker-machine
-  Write-Error "FAIL: docker-machine binary still found"
+  . docker-machine create -d vmwareworkstation test
 } catch {
-  Write-Host "PASS: docker-machine not found"
 }
-
-"TEST: Update from older version to single binary version works"
-. choco install -y docker-machine $options -version 0.5.2
-. choco install -y docker-machine $options -source . -version $version
-. ls C:\programdata\chocolatey\lib\docker-machine
-. ls C:\programdata\chocolatey\lib\docker-machine\tools
-$numExe = (get-childitem -path C:\programdata\chocolatey\lib\docker-machine\tools\ | where { $_.extension -eq ".exe" }).Count
-Write-Host "numExe $numExe"
-$numIgnore = (get-childitem -path C:\programdata\chocolatey\lib\docker-machine\tools\ | where { $_.extension -eq ".ignore" }).Count
-Write-Host "numIgnore $numIgnore"
-if ($numExe - 1 -ne $numIgnore) {
-  Write-Error "FAIL: Wrong number of ignored plugins!"
-}
-if ($numExe -ne 1) {
-  Write-Error "FAIL: There mustn't be more than one exe file!"
-}
-if ($numIgnore -ne 0) {
-  Write-Error "FAIL: There mustn't be any ignored plugins!"
+if (! (Test-Path $env:USERPROFILE\.docker\machine\machines\test)) {
+  Write-Error "FAIL: Machine directory is missing"
 }
 
 "TEST: Uninstall show remove the binary"
-. choco uninstall -y docker-machine
-try {
-  . docker-machine
-  Write-Error "FAIL: docker-machine binary still found"
-} catch {
-  Write-Host "PASS: docker-machine not found"
-}
+. choco uninstall -y docker-machine-vmwareworkstation
 
 "TEST: Finished"
