10d7651eab620bfcd31de45dd97bd6b66d5842ae
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-24 08:27:30 +0100

    Update to version 1.1.0, add update.sh and test.sh
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 $packageName    = 'docker-machine-vmwareworkstation'
 $driverName     = 'docker-machine-driver-vmwareworkstation'
-$url            = 'https://github.com/pecigonzalo/docker-machine-vmwareworkstation/releases/download/v1.0.10/docker-machine-driver-vmwareworkstation.exe'
-$checksum       = 'f5057969ac5b30ff5d1ad1d5d337aa5c'
-$url64          = 'https://github.com/pecigonzalo/docker-machine-vmwareworkstation/releases/download/v1.0.10/docker-machine-driver-vmwareworkstation.exe'
-$checksum64     = 'f5057969ac5b30ff5d1ad1d5d337aa5c'
-$checksumType   = 'md5'
-$checksumType64 = 'md5'
+$url            = 'https://github.com/pecigonzalo/docker-machine-vmwareworkstation/releases/download/v1.1.0/docker-machine-driver-vmwareworkstation.exe'
+$checksum       = '464f7a2d1ef9860721e3e66b90e77d2f1403dc88e259e7be7080db1f8b2c1e1a'
+$url64          = 'https://github.com/pecigonzalo/docker-machine-vmwareworkstation/releases/download/v1.1.0/docker-machine-driver-vmwareworkstation.exe'
+$checksum64     = '464f7a2d1ef9860721e3e66b90e77d2f1403dc88e259e7be7080db1f8b2c1e1a'
+$checksumType   = 'sha256'
+$checksumType64 = 'sha256'
 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageDir  = "$(Split-Path -parent $toolsDir)"
